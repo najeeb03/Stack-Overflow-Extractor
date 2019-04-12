80ccadea6e79339e0c465911dddcb19c92b5c1c6
najeeb03 <najeeb_03@hotmail.com>
2019-04-12 21:49:07 +0600

    JSON data from HTTP GET request
    Successfully made HTTP GET request and parsed and printed out some of the JSON data

--- temp/json-parse-test.py
+++ temp/json-parse-test.py
@@ -1,13 +1,35 @@
+# Testing JSON parsing
+# import json
+
 # Loading form local file to test json parsing
-import json
-
-
-with open('stack-overflow-questions.json') as my_file:
+"""with open('stack-overflow-questions.json') as my_file:
     data = json.load(my_file)
 
 for item in data['items']:
     print ('Question: ' + item['title'])
     print 'By: ' + item['owner']['display_name']
+
     print 'tags: ',
     for tag in item['tags']:
         print tag,
+"""
+
+# Getting the JSON data directly from an API call
+import requests
+
+url_newest = 'http://api.stackexchange.com/2.2/questions?pagesize=10&fromdate=1554508800&todate=1555027200' \
+             '&order=desc&sort=creation&tagged=android&site=stackoverflow'
+url_votes = 'http://api.stackexchange.com/2.2/questions?pagesize=10&fromdate=1554508800&todate=1555027200' \
+           '&order=desc&sort=votes&tagged=android&site=stackoverflow'
+
+r = requests.get(url_votes)
+
+if r.ok:
+    data = r.json()
+
+    for item in data['items']:
+        print ('Question: ' + item['title'])
+        print 'By: ' + item['owner']['display_name']
+
+        print 'tags: ',
+        print ", ".join(str(tag) for tag in item['tags'])
