8292fa8628e07b03c9d6570583572b4e7b13db98
najeeb03 <najeeb_03@hotmail.com>
2019-04-12 19:25:10 +0600

    Small modifications in variable name

--- temp/json-parse-test.py
+++ temp/json-parse-test.py
@@ -5,9 +5,9 @@ import json
 with open('stack-overflow-questions.json') as my_file:
     data = json.load(my_file)
 
-for items in data['items']:
-    print ('Question: ' + items['title'])
-    print 'By: ' + items['owner']['display_name']
+for item in data['items']:
+    print ('Question: ' + item['title'])
+    print 'By: ' + item['owner']['display_name']
     print 'tags: ',
-    for tags in items['tags']:
-        print tags,
+    for tag in item['tags']:
+        print tag,
